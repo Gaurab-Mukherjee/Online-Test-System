084df4c3f40649e03a0fd5470eb235728aa7aee0
Gaurab Mukherjee <68054776+Gaurab-Mukherjee@users.noreply.github.com>
2021-12-30 01:19:47 +0530

    03.12.2021

--- Js/LoginPage.js
+++ Js/LoginPage.js
@@ -1,28 +1,45 @@
-document.getElementById("loginForm").addEventListener("submit",(event)=>{
-  event.preventDefault()
+document.getElementById("loginGoogleForm").addEventListener("submit", (event) => {
+    event.preventDefault()
 })
 
-function googleLogin(){
-  let provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth()
-      .signInWithPopup(provider)
-      .then((result) => {
-        let credential = result.credential;
-
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        let token = credential.accessToken;
-        // The signed-in user info.
-        let user = result.user;
+document.getElementById("loginFacebookForm").addEventListener("submit", (event) => {
+    event.preventDefault()
+})
+//************************************
+// Google Login **********************
+//************************************
+function googleLogin() {
+    let provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+            let credential = result.credential;
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            let token = credential.accessToken;
+            // The signed-in user info.
+            let user = result.user;
+            let uid = result.uid;
+            // fetchUser(credential, token, user, result.uid)
+        }).catch((error) => {
+        // Handle Errors here.
+        let errorCode = error.code;
+        let errorMessage = error.message;
+        // The email of the user's account used.
+        let email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        let credential = error.credential;
         // ...
-
-      }).catch((error) => {
-    // Handle Errors here.
-    let errorCode = error.code;
-    let errorMessage = error.message;
-    // The email of the user's account used.
-    let email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    let credential = error.credential;
-    // ...
-  });
+    });
 }
+//************************************
+// Facebook Login **********************
+//************************************
+function facebookLogin(){
+
+}
+
+function fetchUser(credential, token, user, uid) {
+
+}
+
+
