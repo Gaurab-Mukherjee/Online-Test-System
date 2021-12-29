e8266461b6c4fdbbba1ad578ea37a4ad2e67b9b6
Gaurab Mukherjee <68054776+Gaurab-Mukherjee@users.noreply.github.com>
2021-12-29 13:50:54 +0530

    03.12.2021

---
+++ Js/LoginPage.js
@@ -0,0 +1,28 @@
+document.getElementById("loginWithGoogle").addEventListener("submit",(event)=>{
+  event.preventDefault()
+})
+
+function googleLogin(){
+  let provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+
+        let credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        let token = credential.accessToken;
+        // The signed-in user info.
+        let user = result.user;
+        // ...
+      }).catch((error) => {
+    // Handle Errors here.
+    let errorCode = error.code;
+    let errorMessage = error.message;
+    // The email of the user's account used.
+    let email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    let credential = error.credential;
+    // ...
+  });
+}
---
+++ Js/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "../node_modules/firebase/compat";
+const firebaseConfig = {
+    apiKey: "AIzaSyCmQSird5PmlaN89gGEiOMRNxdDG_v8YNw",
+    authDomain: "onlinetestsystem-a4cc4.firebaseapp.com",
+    databaseURL: "https://onlinetestsystem-a4cc4-default-rtdb.firebaseio.com",
+    projectId: "onlinetestsystem-a4cc4",
+    storageBucket: "onlinetestsystem-a4cc4.appspot.com",
+    messagingSenderId: "822940975771",
+    appId: "1:822940975771:web:095882336ba022391cfc3e",
+    measurementId: "G-RW6YB5LR7D"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.getAnalytics();
